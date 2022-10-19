4a1c10c8906f46d7e8e3743dfd615587b719853b
Bhangure <bhangureasha853@gmai.com>
2022-10-19 07:13:45 +0530

    i need to push my changes

---
+++ src/main/java/com/MvnApplication/MvnApplication/MvnApplication.java
@@ -0,0 +1,16 @@
+package com.MvnApplication.MvnApplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MvnApplication.class, args);
+		System.out.println("hello world");
+	}
+
+
+
+}
